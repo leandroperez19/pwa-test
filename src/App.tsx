cd673377b4cdf91d21cc676cee2f83e2232cc3e6
Leandro Pérez <perezbarahonaleandro@gmail.com>
2022-08-24 17:19:30 -0400

    service worker ready to test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function App() {
+
+  const [isReadyForInstall, setIsReadyForInstall] = React.useState(false);
+
+  useEffect(() => {
+  window.addEventListener("beforeinstallprompt", (event) => {
+    // Prevent the mini-infobar from appearing on mobile.
+    event.preventDefault();
+    console.log("👍", "beforeinstallprompt", event);
+    // Stash the event so it can be triggered later.
+    (window as any).deferredPrompt = event;
+    // Remove the 'hidden' class from the install button container.
+    setIsReadyForInstall(true);
+  });
+  }, []);
+ 
+  const downloadApp = async () =>{
+    console.log("👍", "butInstall-clicked");
+    const promptEvent = (window as any).deferredPrompt;
+    if (!promptEvent) {
+      // The deferred prompt isn't available.
+      console.log("oops, no prompt event guardado en window");
+      return;
+    }
+    // Show the install prompt.
+    promptEvent.prompt();
+    // Log the result
+    const result = await promptEvent.userChoice;
+    console.log("👍", "userChoice", result);
+    // Reset the deferred prompt variable, since
+    // prompt() can only be called once.
+    (window as any).deferredPrompt = null;
+    // Hide the install button.
+    setIsReadyForInstall(false);
+  }
+
   return (
     <div className="App">
-      
+      <span>hola</span>
+      {isReadyForInstall && <button onClick={downloadApp}>descargar</button>}
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,6 +12,15 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+serviceWorkerRegistration.register({
+  onUpdate: async (registration : any) => {
+    // Corremos este código si hay una nueva versión de nuestra app
+    // Detalles en: https://developers.google.com/web/fundamentals/primers/service-workers/lifecycle
+    if (registration && registration.waiting) {
+      await registration.unregister();
+      registration.waiting.postMessage({ type: "SKIP_WAITING" });
+      // Des-registramos el SW para recargar la página y obtener la nueva versión. Lo cual permite que el navegador descargue lo nuevo y que invalida la cache que tenía previamente.
+      window.location.reload();
+    }
+  },
+});
